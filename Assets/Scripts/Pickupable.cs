0698258709415299e9c2d6600d41f394e54d10d4
zinnfandel <nmhadas@gmail.com>
2014-01-25 00:55:39 +0200

    Fixed compile error

--- Assets/Scripts/Pickupable.cs
+++ Assets/Scripts/Pickupable.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Pickupable : MonoBehaviour 
 {
-	public PlayerController.CharacterView PickedBy;
+	public CharacterView PickedBy;
 	// Use this for initialization
 	void Start () 
 	{
