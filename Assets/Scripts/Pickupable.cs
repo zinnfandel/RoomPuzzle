1dbcc239d288c2a7646171427df459887655abcd
Lunarsong <shanee.nishry@gmail.com>
2014-01-25 16:51:02 +0200

    Mouse

--- Assets/Scripts/Pickupable.cs
+++ Assets/Scripts/Pickupable.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Pickupable : MonoBehaviour 
 {
 	public CharacterView PickedBy;
+	public bool			InvisibleToOthers = false;
+
 	bool	mWasPicked = false;
 
 
@@ -11,6 +13,30 @@ public class Pickupable : MonoBehaviour
 	void Start () 
 	{
 		mWasPicked = false;
+
+		if ( InvisibleToOthers )
+		{
+			SpriteRenderer[] sprites = GetComponentsInChildren< SpriteRenderer >();
+			foreach ( SpriteRenderer sprite in sprites )
+			{
+				Debug.Log( sprite.sortingLayerName );
+				if ( PickedBy == CharacterView.Child && sprite.gameObject.layer != LayerMask.NameToLayer( "Kid" ) )
+				{
+					sprite.enabled = false;
+				}
+
+				else if ( PickedBy == CharacterView.Cat && sprite.gameObject.layer != LayerMask.NameToLayer( "Cat" ) )
+				{
+					sprite.enabled = false;
+				}
+
+				else if ( PickedBy == CharacterView.Grandma && sprite.gameObject.layer != LayerMask.NameToLayer( "Grandma" ) )
+				{
+					sprite.enabled = false;
+				}
+			}
+
+		}
 	}
 	
 	// Update is called once per frame
@@ -32,6 +58,12 @@ public class Pickupable : MonoBehaviour
 			mWasPicked = true;
 			transform.position = TableSpot.GetTableSpot();
 			Debug.Log ( "Picked!" );
+
+			SpriteRenderer[] sprites = GetComponentsInChildren< SpriteRenderer >();
+			foreach ( SpriteRenderer sprite in sprites )
+			{
+				sprite.enabled = true;
+			}
 		}
 	}
 }
