2e83e8acec80bcdc91b6d5f3ce797985affd030c
elib <ewokker@gmail.com>
2014-01-25 11:32:27 +0200

    Added sounds and sound player.

---
+++ Assets/Scripts/PlaySoundEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundEvent : GameEvent {
+
+	public AudioClip ClipToPlay { get; set; }
+
+	public PlaySoundEvent(AudioClip theClip)
+	{
+		ClipToPlay = theClip;
+	}
+}
---
+++ Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundPlayer : MonoBehaviour {
+
+	public AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		Events.instance.AddListener<PlaySoundEvent>(OnPlaySound);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnPlaySound(PlaySoundEvent e)
+	{
+		source.PlayOneShot(e.ClipToPlay);
+	}
+}
