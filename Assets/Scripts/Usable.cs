a50bdfe64769a851f02b3db70952229649796f74
Lunarsong <shanee.nishry@gmail.com>
2014-01-25 17:33:52 +0200

    Updated scene

--- Assets/Scripts/Usable.cs
+++ Assets/Scripts/Usable.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Usable : MonoBehaviour {
-
+public class Usable : MonoBehaviour 
+{
+	public Pickupable UsedWith = null;
+	public GameObject Reveals = null;
+	public CharacterView UsedBy;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log( "Usable clicked" );
+		if ( PlayerController.s_PlayerController.GetView() == UsedBy )
+		{
+			Debug.Log( "Usable by right user" );
+			if ( UsedWith == null || UsedWith == Pickupable.GetCurrent() )
+			{
+				Debug.Log( "Usable activated" );
+				if ( Reveals )
+				{
+					Reveals.SetActive( true );
+				}
+			}
+		}
 	}
 }
